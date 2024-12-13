38c544b968b27db0e114d0316bdbf9775d545a29
RinnyD <oldacc1020@gmail.com>
2024-12-13 22:08:39 +0700

    df

--- modules/login_function.py
+++ modules/login_function.py
@@ -1,1 +1,9 @@
 
+from sub_main import Login, UserLogin
+def login_function():
+    user_login = Login()
+    user_login.UserLogin()
+    print("\nLogin Complete!")
+    print("Thank you for using our system!\n")
+    print("="*50)
+    print("\n")
