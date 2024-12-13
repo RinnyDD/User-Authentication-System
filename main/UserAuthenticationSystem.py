f97b33752564ba4330850568c88d48d5f8b27633
Ozzy-wayne <Vansophat7@gmail.com>
2024-12-13 21:42:31 +0700

    new

--- main/UserAuthenticationSystem.py
+++ main/UserAuthenticationSystem.py
@@ -4,9 +4,22 @@ import os
 
 # Add the modules folder to sys.path
 sys.path.append(os.path.abspath(r"D:\Vs python\github user\User-Authentication-System\modules"))
-from ..sub_main import Login
+from register_function import register_function
 
+def main():
+    while True:
+        print("1. Register\n2. Login\n3. Password Recovery\n4. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            register_function()
 
-login_system = Login()
-login_system.login()
-
+        '''elif choice == "2":
+            login_system = Login()
+            login_system.login()
+        elif choice == "3":
+            PasswordRecovery()
+        
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice.")'''
