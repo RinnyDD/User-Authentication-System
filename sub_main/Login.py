64c3c723a5bbe981c2b6ba813c6e8ebd10076ffc
RinnyD <oldacc1020@gmail.com>
2024-12-14 09:45:38 +0700

    ff

--- sub_main/Login.py
+++ sub_main/Login.py
@@ -1,17 +1,21 @@
+
+import sys
 import random
 import maskpass
 import os
 import time
-import sys
 
 sys.path.append(os.path.abspath(r"..\Python In CyberSecurity\ProposalProject\User-Authentication-System\main"))
 
-COOLDOWN_TIME_LOGIN = 60
+COOLDOWN_TIME_USERNAME = 6
+COOLDOWN_TIME_LOGIN = 6
 COOLDOWN_TIME_CODE = 30
 MAX_ATTEMPTS = 3
+MAX_ERROR = 5
+COOLDOWN_ERROR = 20
 
 class UserLogin:
-    def __init__(self, database="database.txt"):
+    def __init__(self, database="register_user.txt"):
         script_directory = os.path.dirname(os.path.abspath(__file__))
         self.database = os.path.join(script_directory, database)
 
@@ -61,26 +65,38 @@ class UserLogin:
         print("Welcome to the Login System!")
         
         while True:
+            
             # Prompt for username
-            while True:
+            attempts_username = 0
+            
+            while attempts_username < MAX_ATTEMPTS:
                 username = input("Enter username: ").strip()
                 verification_result = self.verify(username, "")
                 
                 if verification_result == "username_invalid":
+                    attempts_username += 1
                     print("Please try again.")
+                    print(f"Remaining attempts: {MAX_ATTEMPTS - attempts_username}")
+                    
+                    if attempts_username == MAX_ATTEMPTS:
+                        print("Please Becareful next time.")
+                        for remaining_time in range(COOLDOWN_TIME_USERNAME, 0, -1):
+                            print(f"Cooldown time remaining: {remaining_time} seconds", end="\r")
+                            time.sleep(1)
+                        return
                     
                 else:
                     break
             
+            # Prompt for password
             attempts_login = 0
             
-            # Prompt for password
             while attempts_login < MAX_ATTEMPTS:
                 password = maskpass.askpass(mask='').strip()
                 verification_result = self.verify(username, password)
                 
                 if verification_result == "password_invalid":
-                    print("!!! Wrong password !!!")
+                    print("!!!  Wrong password  !!!")
                     print("!!! Please try again !!!")
                     
                 elif verification_result is True:
@@ -92,22 +108,22 @@ class UserLogin:
                 
                 attempts_login += 1
                 print(f"Remaining attempts: {MAX_ATTEMPTS - attempts_login}")
-            print("Too many failed attempts. Access denied.")
-            if attempts_login >= MAX_ATTEMPTS:
-                print("Please wait before trying again.")
+                if attempts_login == MAX_ATTEMPTS:
+                    print("Please wait before trying again.")
                 
-                for remaining_time in range(COOLDOWN_TIME_LOGIN, 0, -1):
-                    print(f"Cooldown time remaining: {remaining_time} seconds", end="\r")
-                    time.sleep(1)
-                return
+                    for remaining_time in range(COOLDOWN_TIME_LOGIN, 0, -1):
+                        print(f"Cooldown time remaining: {remaining_time} seconds", end="\r")
+                        time.sleep(1)
+                    break
+            print("Too many failed attempts. Access denied.")
 
     def verify_code_flow(self):
         code = self.generate_random_code()
         print("Your verification code is:{}".format(code))
         attempts_code_retype = 0
         attempts_code_resend = 0
-        
-        while attempts_code_resend < MAX_ATTEMPTS or attempts_code_retype < MAX_ATTEMPTS:
+        error = 0
+        while attempts_code_resend < MAX_ATTEMPTS or attempts_code_retype < MAX_ATTEMPTS or error < MAX_ATTEMPTS:
             verify_code = input("Enter code: ").strip().upper()
             
             if verify_code == code:
@@ -119,7 +135,7 @@ class UserLogin:
                     
             while verify_code != code:
                 choices = input("Do you want to [retype], [resend] or [leave] the code ? --> ").strip().lower()
-        
+
                 if choices =='resend' or choices == 'rs':
                     code = self.generate_random_code()
                     print("Your verification code is:{}".format(code))
@@ -132,8 +148,8 @@ class UserLogin:
                             break   
                         print("Please wait before trying again.")
                         attempts_code_resend += 1
+                        print("!!! WRONG CODE !!!")
                         print(f"Remaining attempts: {MAX_ATTEMPTS - attempts_code_resend}")
-                        print("Too many failed attempts. Access denied.")
                         
                         
                         for remaining_time in range(COOLDOWN_TIME_CODE, 0, -1):
@@ -146,7 +162,7 @@ class UserLogin:
                         break
                            
                     if verify_code != code:
-                        print("!!! Wrong code !!!")
+                        print("!!! WRONG CODE !!!")
                             
                 elif choices =='retype' or choices == 'rt':
                     
@@ -155,8 +171,8 @@ class UserLogin:
                         
                         if verify_code != code:
                             attempts_code_retype += 1
+                            print("!!! WRONG CODE !!!")
                             print(f"Remaining attempts: {MAX_ATTEMPTS - attempts_code_retype}")
-                            print("Too many failed attempts. Access denied.")
                                  
                         
                         elif verify_code == code:
@@ -170,11 +186,19 @@ class UserLogin:
                         print("Please resend the code and try again !")
     
                 elif choices == 'leave' or choices == 'l':
-                    print("Thank you for using our service, Have a good day ! ")
+                    print("Thank you for using my service, Have a good day ! ")
                     break
                 else:
                     print("Invalid choice! Please choose the correct choice.")
-                    
+                    error += 1
+                    print(f"Remaining attempts: {MAX_ERROR - error}")
+                    if error == MAX_ERROR:
+                        error = 0
+                        print("You spam too much, We now block you from login to your account!")
+                        for remaining_time in range(COOLDOWN_ERROR, 0, -1):
+                            print(f"Cooldown time remaining: {remaining_time} seconds", end="\r")
+                            time.sleep(1)
+                        continue
             return
 
 # Create an instance of the Login class and call the login process
